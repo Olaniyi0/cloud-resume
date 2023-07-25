cffbcfb06b00512074bf34f1c9c75207ebdda18e
olaniyi <niyioladoja0@gmail.com>
2023-07-25 20:26:58 +0100

    Added Function to the mix

--- locals.tf
+++ locals.tf
@@ -2,5 +2,5 @@ locals {
   storage_account_name = "${random_pet.pet.id}${var.storage_account_name}"
   pet_name = random_pet.pet.id
   sas_start_time = timestamp()
-  sas_expiry_time = timeadd(local.sas_start_time, "1h")
+  sas_expiry_time = timeadd(local.sas_start_time, "30m")
 }
